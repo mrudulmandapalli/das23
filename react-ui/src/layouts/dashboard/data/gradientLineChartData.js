ff5d5e5b3b7d3d0cba1b1c5a801274d2271c75a2
mrudulmandapalli <mrudulsai8888@gmail.com>
2024-08-07 15:54:39 -0500

    first commit

---
+++ react-ui/src/layouts/dashboard/data/gradientLineChartData.js
@@ -0,0 +1,15 @@
+export default {
+  labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+  datasets: [
+    {
+      label: "Mobile apps",
+      color: "info",
+      data: [55, 47, 350, 220, 600, 150, 300, 830, 600],
+    },
+    {
+      label: "Websites",
+      color: "dark",
+      data: [60, 90, 55, 450, 295, 280, 730, 130, 400],
+    },
+  ],
+};
